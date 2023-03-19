494f308bb16f9cb7c35e1c0da2ba712b19c17445
André Campelo <decocampeloreis@gmail.com>
2023-03-18 22:43:40 -0300

    :zap: perf: adicionando obrigatoriedade de conexão

--- src/sender.ts
+++ src/sender.ts
@@ -1,8 +1,15 @@
 import parsePhoneNumber, {isValidPhoneNumber} from "libphonenumber-js"
 import { create, Whatsapp, Message, SocketState } from "venom-bot"
 
+
+export type QRcode = {
+    base64Qr: string
+}
+
 class Sender {
     private client: Whatsapp
+    private isConnected: string
+    private qr: QRcode
 
     constructor() {
         this.initialize()
@@ -28,10 +35,14 @@ class Sender {
     }
 
     private initialize() {
-        
+        const qr = (base64Qr: string) => {
+            this.qr = { base64Qr }
+        }
+
+
         const start = (client: Whatsapp) => {
             this.client = client
-
+   
             // this.sendText("5544998973198@c.us", "Olá, essa é uma mensagem automática")
         }
 
